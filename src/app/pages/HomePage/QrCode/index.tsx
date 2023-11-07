5a66662100e9e6bf3f5ccdfd075743a79479080e
Nguyễn Đắc Quang <nguyendacqunang@Buis-MacBook-Pro.local>
2023-11-07 18:02:00 +0700

    update

--- src/app/pages/HomePage/QrCode/index.tsx
+++ src/app/pages/HomePage/QrCode/index.tsx
@@ -2,14 +2,11 @@ import * as React from 'react';
 import styled from 'styled-components/macro';
 import { Col, Row } from 'antd';
 import Donate3 from 'images/donate3.png';
-import { useScrollDirection } from 'react-use-scroll-direction'
 
 export function QrCode() {
-  const { isScrollingDown } = useScrollDirection();
   return (
     <Wrapper>
       <Row className="qr_code" justify="space-around" align="middle">
-        {isScrollingDown ? 'Scrolling down' : 'scrolling up'}
         <img src={Donate3} />
       </Row>
     </Wrapper>
