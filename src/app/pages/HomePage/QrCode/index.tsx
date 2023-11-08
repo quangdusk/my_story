0258c3f7a861b1d139aeba96486411cd99a60fe6
quangnd <ndquangtt1@gmail.com>
2023-11-08 22:22:28 +0700

    update

--- src/app/pages/HomePage/QrCode/index.tsx
+++ src/app/pages/HomePage/QrCode/index.tsx
@@ -1,37 +1,94 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components/macro';
-import { Col, Row } from 'antd';
+import { Modal, QRCode } from 'antd';
 import Donate3 from 'images/donate3.png';
+import VBa from 'images/v-ba.png';
+import VBL from 'images/v-l.png';
+import VBR from 'images/v-r.png';
+import VB from 'images/v-b.png';
 
 export function QrCode() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <Wrapper>
-      <ul>
-        <li><img src={Donate3} /></li>
-      </ul>
+      <div className="qr_code" onClick={showModal}>
+        <ul>
+          <li >
+            <img src={Donate3}/>
+          </li>
+        </ul>
+        <div className="letter">
+          <div className="letter_image">
+            <img className="letter_center" src={VBa} />
+            <img className="letter_left" src={VBL} />
+            <img className="letter_right" src={VBR} />
+            <img className="letter_top" src={VB} />
+          </div>
+        </div>
+      </div>
+
+      <Modal
+        // title="Basic Modal"
+        open={isModalOpen}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        <QRCode
+          errorLevel="H"
+          size={160}
+          iconSize={160 / 4}
+          value="https://ant.design/"
+          icon="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
+        />
+      </Modal>
     </Wrapper>
   );
 }
 
 const Wrapper = styled.div`
   margin-top: 100px;
-  ul {
+  .qr_code {
     margin: 0;
     padding: 0;
     position: fixed;
     right: 30px;
     bottom: 30px;
   }
-  
+  .letter {
+    position: absolute;
+    top: 38%;
+    left: 45%;
+  }
+  .letter_image {
+    position: relative;
+  }
+
+  .letter_left {
+    position: absolute;
+    left: 0;
+  }
+  .letter_right {
+    position: absolute;
+    left: 48%;
+  }
+
+  .letter_top {
+    position: absolute;
+    transform: rotate(180deg);
+    left: 0;
+  }
+
   ul li {
-    background-color: red;
     list-style: none;
-    width: 120px;
-    height: 120px;
-    border-radius: 50%;
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.5);
     animation: animate 7s linear infinite;
-    /* transform-origin: bottom center; */
   }
 
   @keyframes animate {
